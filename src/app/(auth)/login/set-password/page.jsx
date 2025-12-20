66c6ba3f665bb929c9783934399e40eeccf023a6
alaminislam34 <mi3548514@gmail.com>
2025-12-21 03:14:35 +0600

    added something

--- src/app/(auth)/login/set-password/page.jsx
+++ src/app/(auth)/login/set-password/page.jsx
@@ -36,17 +36,25 @@ const InputField = ({
     />
   </div>
 );
-function VerifyOTP() {
-  const [otp, setOTP] = useState("");
+function ForgotPassword() {
+  const [formData, setFormData] = useState({
+    identifier: "",
+    password: "",
+  });
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
     try {
-      router.push("/login/set-password");
+      router.push("/login/verify-otp");
     } catch (error) {
       console.error("Login Error:", error);
       toast.error("Internal Server Error. Try again later.");
@@ -57,26 +65,23 @@ function VerifyOTP() {
 
   return (
     <div className="max-w-2xl w-full bg-white dark:bg-white/3 border border-gray-200 dark:border-white/10 backdrop-blur-xl rounded-3xl shadow-2xl shadow-Primary/20 dark:shadow-Primary/10 p-6 md:p-10">
-      <Link href={"/login/forgot-pass"}>
+      <Link href={"/login"} className={``}>
         <ArrowLeft />
       </Link>
       <header className="text-center py-2 my-6">
         <h1 className="text-4xl font-extrabold leading-normal text-gray-900 dark:text-transparent dark:bg-clip-text dark:bg-linear-to-b dark:from-white dark:to-gray-400">
-          Verify OTP{" "}
+          Forgot your password
         </h1>
-        <p className="text-gray-400 py-2">
-          We send a code your gamil please check!
-        </p>
       </header>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-6">
         <InputField
-          label="OTP"
-          name="otp"
+          label="Email or Phone"
+          name="identifier"
           type="text"
-          placeholder="enter your otp.."
-          value={otp}
-          onChange={(v) => setOTP(v.target.value)}
+          placeholder="example@mail.com"
+          value={formData.identifier}
+          onChange={handleChange}
         />
 
         <div className="flex items-center gap-6">
@@ -87,7 +92,7 @@ function VerifyOTP() {
               loading ? "bg-gray-400" : "bg-orange-500 hover:opacity-90"
             } text-white font-bold rounded-xl active:scale-[0.98] transition-all shadow-lg shadow-orange-500/20 mt-2`}
           >
-            {loading ? "Verify..." : "Verify Now"}
+            {loading ? "Logging in..." : "Login"}
           </button>
         </div>
       </form>
@@ -95,4 +100,4 @@ function VerifyOTP() {
   );
 }
 
-export default VerifyOTP;
+export default ForgotPassword;
